3829add2fe78fcf1fec935f225796a3c7b732df5
Elie Harfouche <elie7795@gmail.com>
2023-02-01 23:04:06 +0200

    install linters

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get 'message/index'
 
   resources :messages
-
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,9 +7,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Message.create(message: "Hello!")
-Message.create(message: "Hi there.")
-Message.create(message: "Good morning.")
-Message.create(message: "Good afternoon.")
-Message.create(message: "Good evening.")
+Message.create(message: 'Hello!')
+Message.create(message: 'Hi there.')
+Message.create(message: 'Good morning.')
+Message.create(message: 'Good afternoon.')
+Message.create(message: 'Good evening.')
 Message.create(message: "It's nice to meet you.")
